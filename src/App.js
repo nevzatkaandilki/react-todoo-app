dd9bc6b05fae2a9553cf96c7776f897aa2cd2b0b
Nevzat Kaan Dilki <nevzatkaan46@gmail.com>
2024-07-16 11:32:15 +0300

    react todoo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import TodoList from "./components/TodoList";
+import AddTodo from "./components/AddTodo";
+
+
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, todo]);
+  };
+
+  const toggleTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="title" > To-do List </h1>
+      <AddTodo addTodo={addTodo} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </div>
   );
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function TodoItem({todo, index, toggleTodo, deleteTodo}) {
+    return (
+        <li style= {{ textDecoration:todo.completed ? "line-through": "" }}>
+        {todo.text}
+        <button onClick={() =>toggleTodo(index)}>
+            {todo.completed ? "Undo" : "Complete"}
+            </button>    
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+        </li> 
+    );
+}
+
+export default TodoItem;
